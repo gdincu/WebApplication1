d5aa056bd21e482769a94655ed215316d70f92e1
Guest1 <guest1@netrom.live>
2020-07-07 12:56:11 +0300

    Updated fetch-data component

--- WebApplication1/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ WebApplication1/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,22 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public tires: Tire[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<Tire[]>(baseUrl + 'api/Tires').subscribe(result => {
+      this.tires = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Tire {
+  id: number;
+  typeId: number
+  manufacturerId: number;
+  styleId: number;
+  width: number;
+  height: number;
+  rimSize: number;
+  locationId: number;
 }
